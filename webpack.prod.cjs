011f14697b70fe5aed2350aa99b5a7a882d170f9
JosephAshwin23 <joseph.sanjivi@anywhere.co>
2024-02-18 22:54:32 +0530

    removed dotenv plugin and added zip plugin

--- webpack.prod.cjs
+++ webpack.prod.cjs
@@ -1,7 +1,15 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.cjs');
+var ZipPlugin = require('zip-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [
+    new ZipPlugin({
+      filename: 'eds-checker.zip',
+      path: path.resolve(__dirname, 'chrome-deploy')
+    }),
+  ]
 });
