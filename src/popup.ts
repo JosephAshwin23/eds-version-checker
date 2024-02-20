f1fb3b26db72f24746ee9de5205c9a3a6ae40855
JosephAshwin23 <joseph.sanjivi@anywhere.co>
2024-02-20 22:08:12 +0530

    added extension id based on env

--- src/popup.ts
+++ src/popup.ts
@@ -3,7 +3,10 @@ import '../styles/popup.css';
 addEventListener('DOMContentLoaded', () => {
   const toggleBtn = document.getElementById('switch') as HTMLInputElement;
 
-  const id = 'llfiddgnhcoieondlcacmjnndphkdkoc';
+  const id =
+    process.env.NODE_ENV === 'development'
+      ? 'llfiddgnhcoieondlcacmjnndphkdkoc'
+      : 'koggbkcbcigigcmjomnnhmjhgbaleool';
 
   toggleBtn.addEventListener('click', () => {
     chrome.management.setEnabled(id, false);
